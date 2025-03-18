12058c3c05132276bfc39eae714667fc35741d72
Kayvin59 <kayvin4@hotmail.com>
2025-03-18 13:23:22 +0100

    feat: add remotion

---
+++ src/remotion/Composition.tsx
@@ -0,0 +1,15 @@
+
+export const MyComposition = () => {
+    return null;
+/*     const { width } = useVideoConfig();
+
+
+    return (
+      <AbsoluteFill>
+        <AbsoluteFill style={{ width: width/2 }}>
+        </AbsoluteFill>
+        <AbsoluteFill style={{ left: width/2 }}>
+        </AbsoluteFill>
+      </AbsoluteFill>
+    ) */
+};

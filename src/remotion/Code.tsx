5ff8e95e9498d713356f28dd3a69ff7a357d218c
Kayvin59 <kayvin4@hotmail.com>
2025-03-29 15:00:18 +0100

    feat: add prism + highlighted code

--- src/remotion/Code.tsx
+++ src/remotion/Code.tsx
@@ -1,9 +1,19 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { solarizedDarkAtom } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 export function Code({ code }: { code: string }) {
   return (
-    <div className="overflow-auto rounded-lg bg-[#1e1e1e] p-4·font-mono text-sm">
+/*     <div className="overflow-auto rounded-lg bg-[#1e1e1e] p-4·font-mono text-sm">
       <pre className="!m-0">
         <code className="language-jsx">{code}</code>
       </pre>
-    </div>
+    </div> */
+    <SyntaxHighlighter
+      language="typescript"
+      style={solarizedDarkAtom}
+      showLineNumbers
+    >
+      {code}
+    </SyntaxHighlighter>
   );
 }

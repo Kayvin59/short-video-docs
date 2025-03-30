b89ea4b1ab94e7b48c5139266646650c9965295d
Kayvin59 <kayvin4@hotmail.com>
2025-03-30 14:52:39 +0200

    style: update sytax highlight theme

--- src/remotion/Code.tsx
+++ src/remotion/Code.tsx
@@ -1,19 +1,16 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { solarizedDarkAtom } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { materialOceanic } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export function Code({ code }: { code: string }) {
   return (
-/*     <div className="overflow-auto rounded-lg bg-[#1e1e1e] p-4·font-mono text-sm">
-      <pre className="!m-0">
-        <code className="language-jsx">{code}</code>
-      </pre>
-    </div> */
-    <SyntaxHighlighter
-      language="typescript"
-      style={solarizedDarkAtom}
-      showLineNumbers
-    >
-      {code}
-    </SyntaxHighlighter>
+    <>
+      <SyntaxHighlighter
+        language="typescript"
+        style={materialOceanic}
+        showLineNumbers={true}
+      >
+        {code}
+      </SyntaxHighlighter>
+    </>
   );
 }
